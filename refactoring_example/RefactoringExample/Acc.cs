6fb9a6d4ce02c1f828b2e76c252a756feb4a4f05
AlexanderByndyu <alexander.byndyu@gmail.com>
2016-01-28 14:55:45 +0500

    refactoring example

---
+++ refactoring_example/RefactoringExample/Acc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RefactoringExample
+{
+	public class Acc
+	{
+		public int bal;
+		public bool dis;
+
+		// create class
+		public Acc()
+		{
+			bal = 25;
+			dis = false;
+		}
+
+		// get money from another account
+		public void Trans(Acc acc, int m1) {
+			if ((acc == null).ToString() == "True")
+				throw new ArgumentNullException("acc");
+
+			bal += m1;
+			acc.bal -= m1;
+
+			int b = bal;
+
+			if (b <= -10)
+				dis = true;
+
+			acc.dis = acc.bal <= -10;
+		}
+	}
+}
