b040d46b5c4bb70bd033b1ed0f01561ae4a52963
AlexanderByndyu <alexander.byndyu@gmail.com>
2016-01-28 21:05:52 +0500

    add AFT

---
+++ source/AFT/LoginUiTest.cs
@@ -0,0 +1,26 @@
+﻿namespace AFT
+{
+    using NUnit.Framework;
+    using OpenQA.Selenium.Chrome;
+
+    public class LoginUiTest
+    {
+        [Test]
+        public void IfEmailAddressIsNotValid_ShowValidationMessage()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:49333/Account/Login");
+
+                var loginErrorMessage1 = driver.FindElementsByXPath("//*[@id='loginForm']/form/div[1]/div/span/span");
+                Assert.AreEqual(0, loginErrorMessage1.Count);
+
+                driver.FindElementByXPath("//*[@id='loginForm']/form/div[4]/div/input").Click();
+
+                var loginErrorMessage = driver.FindElementByXPath("//*[@id='loginForm']/form/div[1]/div/span/span");
+
+                Assert.True(loginErrorMessage.Displayed);
+            }
+        }
+    }
+}
